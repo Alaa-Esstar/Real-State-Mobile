a9671f5a8db7578592aed4d91bddbf816e628599
Alaa Esstar <alaa.esstar@gmail.com>
2025-03-05 17:06:05 +0100

    first commit

---
+++ lib/appwrite.ts
@@ -0,0 +1,23 @@
+import { Account, Avatars, Client } from "react-native-appwrite";
+import * as Linking from "expo-linking";
+
+export const config = {
+    platform: "com.personal.restate",
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
+
+export const client = new Client();
+client.setEndpoint(config.endpoint!).setProject(config.projectId!).setPlatform(config.platform!);
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+
+export async function login() {
+    try {
+        const redirectURL = Linking.createURL("/");
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}

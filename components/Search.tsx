1e64098bc18ba604fce715e4b8673190ef4b7bd6
Alaa Esstar <alaa.esstar@gmail.com>
2025-03-13 23:54:28 +0100

    Enhance property listing with search and no results handling; refactor card components to display dynamic data

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,11 +1,10 @@
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
+import { View, Image, TextInput, TouchableOpacity } from 'react-native'
 import { useState } from 'react'
 import { router, useLocalSearchParams, usePathname } from 'expo-router'
 import icons from '@/constants/icons';
 import { useDebouncedCallback } from 'use-debounce'
 
 const Search = () => {
-    const path = usePathname();
     const params = useLocalSearchParams<{ query?: string }>();
     const [search, setSearch] = useState(params.query);
 
@@ -20,7 +19,7 @@ const Search = () => {
         <View className='flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2'>
             <View className='flex-1 fmex flex-row items-center justify-start z-50'>
                 <Image source={icons.search} className='size-5' />
-                <TextInput value={search} onChangeText={() => handleSearch} placeholder='Search for anything' className='text-sm font-rubik text-black-300 ml-2 flex-1' />
+                <TextInput value={search} onChangeText={handleSearch} placeholder='Search for anything' className='text-sm font-rubik text-black-300 ml-2 flex-1' />
             </View>
             <TouchableOpacity>
                 <Image source={icons.filter} className='size-5' />
